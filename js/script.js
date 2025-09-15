6a1bba7ed7b6c9d2ab18fba304a25f751f530b64
Moon110 <moynul1.islam0@gmail.com>
2025-09-16 00:45:48 +0600

    calculate button functionality added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const calculateButton = document.getElementById('calculate');
+calculateButton.addEventListener('click', function(){
+    const income = parseFloat(document.getElementById('income').value);
+    const software = parseFloat(document.getElementById('software').value);
+    const courses = parseFloat(document.getElementById('courses').value);
+    const internet = parseFloat(document.getElementById('internet').value);
+
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+
+    console.table({totalExpenses, balance});
+    
+})
